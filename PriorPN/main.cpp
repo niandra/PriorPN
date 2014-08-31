e9fb195e591c7dffe0d7aa2cf13e8bb3b0eb33d7
niandra <niandra@MacBook-Pro-di-niandra.local>
2014-08-31 21:21:38 +0200

    Prove prior NIW

---
+++ PriorPN/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  PriorPN
+//
+//  Created by niandra on 27/08/14.
+//  Copyright (c) 2014 niandra. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
